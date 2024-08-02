382eb78567b37cce127254666a6b337ac93c5aa9
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-08-02 10:49:59 +0700

    Use gintreg With (In) R Software

--- gintreg/gintreg.R
+++ gintreg/gintreg.R
@@ -5,7 +5,7 @@
 install.packages("remotes")
 remotes::install_github("ystmmrsw/hintreg")
 library("hintreg")
-gintreg = read.csv("https://raw.githubusercontent.com/timbulwidodostp/gintreg/main/gintreg/gintreg.csv",sep = ";")
+gintreg = read.csv("https://raw.githubusercontent.com/timbulwidodostp/gintreg_r/main/gintreg/gintreg.csv",sep = ";")
 # Estimation Generalized normal interval regression model Use gintreg With (In) R Software
 gintreg[sapply(gintreg,is.character)]<-lapply(gintreg[sapply(gintreg,is.character)],as.factor)
 intreg <- intreg(q2 ~ sex,data = gintreg,thresholds = c(-.5, .5, 1, 2, 3, 4, 5))
